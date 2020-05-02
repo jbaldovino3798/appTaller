86af90c743071396cb5d71691224943f64ee3429
jbaldovino3798 <josemariobaldovino123@gmail.com>
2020-05-02 16:53:17 -0500

    Splash Screen

--- app/src/main/java/com/example/diseoapp/ContentActivity.java
+++ app/src/main/java/com/example/diseoapp/ContentActivity.java
@@ -1,10 +1,13 @@
 package com.example.diseoapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class ContentActivity extends AppCompatActivity {
 
@@ -17,5 +20,30 @@ public class ContentActivity extends AppCompatActivity {
     public void launchFormActivity(View view) {
         Intent intent = new Intent(this, FormActivity.class);
         startActivity(intent);
+    }
+
+    public void toastLearn(View view) {
+        Toast.makeText(getApplicationContext(), "Ha clickeado Learn More.",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    public void alertSweatshirts(View view) {
+        AlertDialog.Builder myAlertBuilder = new AlertDialog.Builder(ContentActivity.this);
+        myAlertBuilder.setTitle("Alert");
+        myAlertBuilder.setMessage("Presione OK para agregar al carrito o Cancelar para seguir viendo los productos:");
+        myAlertBuilder.setPositiveButton("OK", new
+                DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // User clicked OK button.
+                        Toast.makeText(getApplicationContext(), "Se ha añadido el producto Sweatshirts al carrito.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+        myAlertBuilder.show();
+    }
+
+    public void toastActivewear(View view) {
+        Toast.makeText(getApplicationContext(), "Se ha añadido el producto Activewear al carrito.",
+                Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/diseoapp/FormActivity.java
+++ app/src/main/java/com/example/diseoapp/FormActivity.java
@@ -1,9 +1,13 @@
 package com.example.diseoapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity{
 
@@ -12,4 +16,28 @@ public class FormActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
     }
+
+    public void onClickShowAlert(View view) {
+        AlertDialog.Builder myAlertBuilder = new AlertDialog.Builder(FormActivity.this);
+        myAlertBuilder.setTitle("Alert");
+        myAlertBuilder.setMessage("Presione OK para continuar o Cancelar para editar los datos:");
+        myAlertBuilder.setPositiveButton("OK", new
+                DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // User clicked OK button.
+                        Toast.makeText(getApplicationContext(), "Sus datos se han guardado.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+        myAlertBuilder.setNegativeButton("Cancel", new
+                DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // User cancelled the dialog.
+                        Toast.makeText(getApplicationContext(), "Por favor edite sus datos.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+        myAlertBuilder.show();
+    }
+
 }
